2b636a3e5a641021ab8e4d93c772377057a8f1d8
Kristen Norris <krnorris65@gmail.com>
2017-11-16 16:33:14 -0600

    displayed products on page

--- scripts/store.js
+++ scripts/store.js
@@ -1,37 +1,38 @@
 $(document).ready(function() {
 
-//catergories, then products
-    const outputEl = $("#product-list")
+    const outputEl = $("#products-list")
+
 
     $.ajax({
         "url": "data/products.json",
         "method": "GET"
-    }).then(productInfo => {
-
-        const products = productInfo.products
-
+    }).then(
+        function (productInfo) {
+        
+            const products = productInfo.products;
+            let productString = ""
 
         $.ajax({
             "url": "data/categories.json",
             "method": "GET"
-        }).then(categoriesInfo => {
-        
-            const categories = categoriesInfo.categories
-            
-            let productString = ""
+        }).then(
+            function (categoriesInfo) {
 
-            products.forEach( product =>
-                productString += `
-                    <h1>${product.name}</h1>
-                    <p>Price: ${product.price}</p>
-                    <p>Category: ${categories.name}</p>
-                `
-            )
+                const categories = categoriesInfo.categories;
+
+            products.forEach( 
+                function (currentP) {
+                    console.log(currentP)
+                    productString += `
+                        <h1>${currentP.name}</h1>
+                        <p>Price: ${currentP.price}</p>
+                    `
+            })
 
             
+            outputEl.html(productString)
         })
-        outputEl.html(productString)
     })
 
-})
+});
 
