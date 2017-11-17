c8dab7d3330d5816da03221fafbcac1ddb1a4c48
Kristen Norris <krnorris65@gmail.com>
2017-11-17 09:21:57 -0600

    commented code

--- scripts/store.js
+++ scripts/store.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
     const outputEl = $("#products-list")
+    const discountEl = $("#season-discount")
 
 
     $.ajax({
@@ -9,36 +10,41 @@ $(document).ready(function() {
     }).then(
         function (productInfo) {
         
-            const products = productInfo.products;
-            let productString = ""
+            const products = productInfo.products; //array of products
+            let productString = "" //empty string of product info
+            let discountString = "" //empty string to display discounts
+            
+            $.ajax({
+                "url": "data/categories.json",
+                "method": "GET"
+            }).then(
+                function (categoriesInfo) {
+                    
+                    const categories = categoriesInfo.categories; //array of categories
 
-        $.ajax({
-            "url": "data/categories.json",
-            "method": "GET"
-        }).then(
-            function (categoriesInfo) {
-
-                const categories = categoriesInfo.categories;
-
-            categories.forEach( category => {
-                productString += `
-                    <h1>${category.name}</h1>
-                `
-                products.forEach( 
-                    product => {
-                        if(category.id === product.category_id) {
-                            productString += `
-                                <h3>${product.name}</h3>
-                                <p>Price: ${product.price}</p>
-                          `
-                        }
+                    categories.forEach( category => {
+                        discountString += `
+                            <option>${category.season_discount}<option> //adds discounts option to select
+                        `
+                        productString += `
+                            <h1>${category.name}</h1> //adds category to productString
+                        `
+                        products.forEach( 
+                            product => {
+                                if(category.id === product.category_id) { //adds products to proper category
+                                    productString += `
+                                        <section id="product_${product.id}">
+                                            <h3>${product.name}</h3>
+                                            <p>Price: ${product.price}</p>
+                                        </section>
+                                    `
+                                }
+                        })
+                    })
+                    
+                    outputEl.html(productString) //adds products to DOM
+                    discountEl.html(discountString) //adds discounts to DOM
                 })
-            }
-            
-        )
-            
-            outputEl.html(productString)
-        })
     })
 
 });
